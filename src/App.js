31ed000ff9ac984f9c4dc7c59b300498e04c6764
Artur <helter@wp.pl>
2022-03-24 12:03:22 +0100

    adding actions creators

--- src/App.js
+++ src/App.js
@@ -1,5 +1,24 @@
+import { useSelector, useDispatch } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { actionCreators } from './state/index';
+import './App.css';
+
 function App() {
-  return <div className="App">Starting</div>;
+  const account = useSelector((state) => state.account);
+  const dispatch = useDispatch();
+
+  const { depositMoney, withdrawMoney } = bindActionCreators(
+    actionCreators,
+    dispatch
+  );
+
+  return (
+    <div className="App">
+      <h1>{account}-</h1>
+      <button onClick={() => depositMoney(1000)}>Deposit</button>
+      <button onClick={() => withdrawMoney(1000)}>Withdraw</button>
+    </div>
+  );
 }
 
 export default App;
